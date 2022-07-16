de2613e1ccb15e756fd63828431840c65b7c2848
Neoxyne <imneoxyne@gmail.com>
2022-07-16 17:49:59 +0100

    Initial commit.

---
+++ Source/GameplayMessageRuntime/GameplayMessageRuntime.Build.cs
@@ -0,0 +1,33 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class GameplayMessageRuntime : ModuleRules
+{
+	public GameplayMessageRuntime(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;				
+
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				"Engine",
+				"GameplayTags"
+			}
+		);
+		
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+			}
+		);
+		
+		DynamicallyLoadedModuleNames.AddRange(
+			new string[]
+			{
+			}
+		);
+	}
+}
